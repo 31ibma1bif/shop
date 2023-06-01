eca681a8ea69c499f4d35b441df356fad445dd49
31ibma1bif <127836976+31ibma1bif@users.noreply.github.com>
2023-06-01 17:59:44 +0200

    Main.java

--- ShopSystem/Main.java
+++ ShopSystem/Main.java
@@ -1,3 +1,5 @@
+package Shop;
+
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -6,43 +8,43 @@ import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Manegement {
-	
-	public Manegement() {
-		
+public class Main{
+
+	public Main() {
+
 	}
-	
-	public Manegement(int max) {
-		
+
+	public Main(int max) {
+
 	}
-	
+
 	protected String kundeName; //vorname
 	protected String nachname;
 	protected int maxItemLimit = 5;
 	protected int ausgewaehlteItems;
 	protected ArrayList<String> einkaufswagen = new ArrayList<>();
 	protected ArrayList <Integer> preis = new ArrayList<>();
-	
+
 	ProduktListe list = new ProduktListe();
-	
+
 	ArrayList<String> gekauft = new ArrayList<>();
-	
+
 	public String getName() {
 		return kundeName;
 	}
-	
+
 	public String getId() {
 		return nachname;
 	}
-	
+
 	public void setName(String newName) {
 		kundeName = newName;
 	}
-	
+
 	public void setId(String newNachname) {
 		nachname = newNachname;
 	}
-	
+
 	public static void main(String[] args) {
 		//TODO
 	}

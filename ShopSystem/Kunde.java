f9327da49cb4680154e6af26aa6a954053bd0335
31ibma1bif <127836976+31ibma1bif@users.noreply.github.com>
2023-06-03 15:44:40 +0200

    Kunde.java

--- ShopSystem/Kunde.java
+++ ShopSystem/Kunde.java
@@ -1,1 +1,26 @@
+package Shop;
 
+import java.util.ArrayList;
+
+public class Kunde {
+
+        protected String kundeName; //vorname
+        protected String nachname;
+        protected int maxItemLimit = 5;
+        protected int ausgewaehlteItems;
+        public ArrayList<String> einkaufswagen = new ArrayList<>();
+        protected ArrayList <Integer> preis = new ArrayList<>();
+
+        ProduktListe list = new ProduktListe();
+
+        ArrayList<String> gekauft = new ArrayList<>();
+
+
+        public String getName() {
+            return kundeName;
+        }
+
+        public String getId() {
+            return nachname;
+        }
+}
